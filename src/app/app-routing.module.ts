bbe58440552f3736a1dbacc1f8cd3dcf7a276988
rafsanjani <sadatrafsanjani@gmail.com>
2021-11-08 17:42:48 +0600

    meeting component is dropped

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {MeetingComponent} from "./meeting/meeting.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'meeting', component: MeetingComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
